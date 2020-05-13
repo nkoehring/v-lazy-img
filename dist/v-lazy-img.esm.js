55ce50be791ef3efa3f5686b12cec6b3c26c4956
koehr <n@koehr.in>
2020-05-13 17:48:52 +0200

    upgrade package and build setup

---
+++ dist/v-lazy-img.esm.js
@@ -0,0 +1,55 @@
+function bindEvent(el, type, fn) {
+  if (el.attachEvent) el.attachEvent("on" + type, fn);else el.addEventListener(type, fn, {
+    capture: false,
+    passive: true
+  });
+}
+
+function success(el, path, css) {
+  el.classList.remove('lazy-load-error');
+  el.classList.remove('lazy-load-progress');
+  el.classList.add('lazy-load-success');
+  if (css) el.style.backgroundImage = "url('" + path + "')";else el.src = path;
+}
+
+function loading(el) {
+  el.classList.remove('lazy-load-error');
+  el.classList.remove('lazy-load-success');
+  el.classList.add('lazy-load-progress');
+}
+
+function failure(el) {
+  el.classList.remove('lazy-load-success');
+  el.classList.remove('lazy-load-progress');
+  el.classList.add('lazy-load-error');
+}
+
+var LazyImg = {
+  load_image: function load_image(path, success, failure) {
+    var img = new Image();
+    bindEvent(img, 'load', success);
+    bindEvent(img, 'error', failure);
+    img.src = path;
+  },
+  install: function install(Vue) {
+    Vue.directive('lazy-load', function (el, binding, vnode) {
+      var path = binding.value;
+      loading(el);
+      LazyImg.load_image(path, function () {
+        return success(el, path);
+      }, function () {
+        return failure(el);
+      });
+    }), Vue.directive('lazy-load-bg', function (el, binding, vnode) {
+      var path = binding.value;
+      loading(el);
+      LazyImg.load_image(path, function () {
+        return success(el, path, true);
+      }, function () {
+        return failure(el);
+      });
+    });
+  }
+};
+
+export default LazyImg;
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
+import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
 import nodeResolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
 
 export default {
   input: './src/index.js',
-  output: {
-    file: './dist/index.js',
-    format: 'umd',
-    name: 'VLazyImg'
-  },
+  output: [
+    { file: pkg.browser, format: 'umd', name: pkg.name, plugins: [terser()] },
+    { file: pkg.main, name: pkg.name, format: 'umd' },
+    { file: pkg.module, format: 'esm' }
+  ],
   plugins: [
     babel(),
-    nodeResolve({ mainFields: ['module', 'main'] })
+    nodeResolve({ mainFields: ['module', 'main'] }),
+    filesize({ showBrotliSize: true })
   ]
 }
